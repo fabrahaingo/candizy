5919c5e7aa3dce0556f4e7d7f88fff8b88e447a6
Maxence Tourneux <tourneuxmaxence@gmail.com>
2021-10-05 15:30:10 +0200

    signup : gestion utilisateur unique avec un status dans l'objet data pour récupérer dans le then du front

--- controllers/user.js
+++ controllers/user.js
@@ -8,34 +8,53 @@ var cryptr = new Cryptr(process.env.SECRET);
 //var decstring = cryptr.decrypt(encstring);
 // console.log('2 : '+ decstring);
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
     console.log(req.body)
-    let regexPassword = /[\w.-]{8,16}/
-    if(regexPassword.test(req.body.password)){
+    let emailDecryptCheck = ''
+    await User.find()
+    .then(user => {
+        for (let element of user){
+            emailDecryptCheck = cryptr.decrypt(element.email)
+    
+            if (req.body.email == emailDecryptCheck) {
+                let message = `L'e-mail est déjà enregistré`
+                let data = {
+                    status: 401,
+                    email: false,
+                }
+                return res.json({ message, data : data })
+            }
+        }
+        let regexPassword = /[\w.-]{8,16}/
+        if(regexPassword.test(req.body.password)){
 
-        var emailEncrypted = cryptr.encrypt(req.body.email)
+            let emailEncrypted = cryptr.encrypt(req.body.email)
 
-        console.log('Signup : ' + emailEncrypted)
+            console.log('Signup : ' + emailEncrypted)
 
-        bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                ...req.body,
-                email: emailEncrypted,
-                password: hash,
-                preInscription: false,
-                isAdmin : false
+            bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User({
+                    ...req.body,
+                    email: emailEncrypted,
+                    password: hash,
+                    preInscription: false,
+                    isAdmin : false
+                })
+                user.save()
+                    .then((response) => res.status(201).json({ message: 'Utilisateur créé !', data: response }))
+                    .catch(error => res.status(400).json({ error }))
             })
-            user.save()
-                .then((response) => res.status(201).json({ message: 'Utilisateur créé !', data: response }))
-                .catch(error => res.status(400).json({ error }))
-        })
-        .catch(error => res.status(500).json({ error }))
-  }
-  else{
-    res.status(400).json({ message: 'Un mot de passe doit contenir 8 caractères au minimum !'})
-  }
-};
+        } else {
+            let data = {
+                status: 400,
+                password: false,
+            }
+            res.status(400).json({ message: 'Un mot de passe doit contenir 8 caractères au minimum !', data: data})
+        }
+    })
+    .catch(error => res.status(500).json({ error }))
+}
 
 exports.login = (req, res, next) => {
     console.log('req.body : ', req.body)
